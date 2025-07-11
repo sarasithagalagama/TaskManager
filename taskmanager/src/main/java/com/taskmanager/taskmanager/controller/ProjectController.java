bbd5156f4b1cca9a345b4deb95a6d8ed6ad92f1b
IT24100548 <sarasithagalagama@gmail.com>
2025-07-11 12:48:32 +0530

    **Add taskmanager directory and initial files**
    This commit initializes the `taskmanager/` directory with necessary files and structure for managing tasks. The changes ensure the foundation for task tracking is in place.

---
+++ taskmanager/src/main/java/com/taskmanager/taskmanager/controller/ProjectController.java
@@ -0,0 +1,5 @@
+package com.taskmanager.taskmanager.controller;
+
+public class ProjectController {
+    
+}
---
+++ taskmanager/src/main/java/com/taskmanager/taskmanager/repository/TaskRepository.java
@@ -0,0 +1,5 @@
+package com.taskmanager.taskmanager.repository;
+
+public class TaskRepository {
+    
+}
